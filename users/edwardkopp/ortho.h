460bb4d2dde8d0f1281063a0ad4ea9608dd8f68f
Edward Kopp <edward@edwardkopp.com>
2024-05-23 02:24:45 -0400

    Revert "woops i forgot something"
    This reverts commit 51b96d479357f9c731ef180c04f4442ccef68822.

--- users/edwardkopp/ortho.h
+++ users/edwardkopp/ortho.h
@@ -76,9 +76,3 @@ const uint16_t specialModifiers[] = {
     EK_CGA,
     EK_____
 };
-
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record);
-
-
-layer_state_t layer_state_set_user(layer_state_t state);
