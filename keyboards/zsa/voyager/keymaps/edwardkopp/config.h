7cc4179c541e8d22009061eb79c2072eb6947abb
Edward Kopp <edward@edwardkopp.com>
2024-05-25 12:16:03 -0400

    updated expected hz comment

--- keyboards/zsa/voyager/keymaps/edwardkopp/config.h
+++ keyboards/zsa/voyager/keymaps/edwardkopp/config.h
@@ -16,6 +16,6 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
 
-// Expecting 160hz real-world
+// Expecting 140hz real-world
 #undef MOUSEKEY_INTERVAL
 #define MOUSEKEY_INTERVAL 4
