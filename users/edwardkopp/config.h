af0c0a6041cb071f55a46b18fe4bc7c4d83e9af3
Edward Kopp <edward@edwardkopp.com>
2024-05-30 10:24:05 -0400

    more blind speed adjustments

--- users/edwardkopp/config.h
+++ users/edwardkopp/config.h
@@ -27,11 +27,11 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 // Good mouse
 #define MK_VARIANT MK_TYPE_KINETIC
-#define MK_KINETIC_MOUSE_ACCN 16
-#define MK_KINETIC_MOUSE_DRAG 8
-#define MK_KINETIC_MOUSE_FRIC 16
+#define MK_KINETIC_MOUSE_ACCN 12
+#define MK_KINETIC_MOUSE_DRAG 32
+#define MK_KINETIC_MOUSE_FRIC 64
 #define MK_KINETIC_MOUSE_MAXS 200
-#define MK_KINETIC_WHEEL_ACCN 8
+#define MK_KINETIC_WHEEL_ACCN 4
 #define MK_KINETIC_WHEEL_DRAG 8
 #define MK_KINETIC_WHEEL_FRIC 8
 #define MK_KINETIC_WHEEL_MAXS 8
