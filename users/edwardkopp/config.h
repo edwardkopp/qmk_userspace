8c9ce1ba2dbe3b84a301f892f264a2e049922c28
Edward Kopp <edward@edwardkopp.com>
2024-05-30 13:51:21 -0400

    Revert "modify mouse interval to target 200hz mouse (real-world perf) on both keyboards"
    This reverts commit 9880b0b018a6cdc5764b398ecd3c61390762d79c.

--- users/edwardkopp/config.h
+++ users/edwardkopp/config.h
@@ -55,10 +55,10 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #endif
 
 
-// Somewhat smooth mouse
+// Somewhat smooth mouse (expecting 130hz real-world from Preonic)
 #define MOUSEKEY_INERTIA
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_INTERVAL 4
+#define MOUSEKEY_INTERVAL 7
 #define MOUSEKEY_MAX_SPEED 50
 #define MOUSEKEY_TIME_TO_MAX 70
 #define MOUSEKEY_FRICTION 40
