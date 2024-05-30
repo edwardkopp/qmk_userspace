f1f8bf8aca4e69aeb78ebcc8ea06b6e33670069e
Edward Kopp <edward@edwardkopp.com>
2024-05-30 16:21:08 -0400

    try an even slower starting speed

--- users/edwardkopp/config.h
+++ users/edwardkopp/config.h
@@ -37,8 +37,8 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 // Good mouse
 #define MK_VARIANT MK_TYPE_KINETIC
-#define MK_KINETIC_MOUSE_ACCN 12
-#define MK_KINETIC_MOUSE_DRAG 8
+#define MK_KINETIC_MOUSE_ACCN 6
+#define MK_KINETIC_MOUSE_DRAG 4
 #define MK_KINETIC_MOUSE_FRIC 32
 #define MK_KINETIC_MOUSE_MAXS 196
 
