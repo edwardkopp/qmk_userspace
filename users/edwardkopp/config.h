dc1454674f4e886e43b98441181bf01a177ade7b
Edward Kopp <edward@edwardkopp.com>
2024-05-30 17:56:40 -0400

    increase mouse and wheel drag

--- users/edwardkopp/config.h
+++ users/edwardkopp/config.h
@@ -38,12 +38,12 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 // Good mouse
 #define MK_VARIANT MK_TYPE_KINETIC
 #define MK_KINETIC_MOUSE_ACCN 12
-#define MK_KINETIC_MOUSE_DRAG 8
+#define MK_KINETIC_MOUSE_DRAG 32
 #define MK_KINETIC_MOUSE_FRIC 32
 #define MK_KINETIC_MOUSE_MAXS 196
 
 // Good mousewheel
 #define MK_KINETIC_WHEEL_ACCN 16
-#define MK_KINETIC_WHEEL_DRAG 8
+#define MK_KINETIC_WHEEL_DRAG 16
 #define MK_KINETIC_WHEEL_FRIC 16
 #define MK_KINETIC_WHEEL_MAXS 1
