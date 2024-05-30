2d37fdeb071ffbec18abdc35e37dd2acd3a57094
Edward Kopp <edward@edwardkopp.com>
2024-05-30 11:01:29 -0400

    fix keycode mixup

--- keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
+++ keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_RAT] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         XXXXXXX, KC_VOLU, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_MS_U, KC_END,  KC_MPRV, KC_DEL,
-        KC_ESC,  KC_MUTE, KC_ALC0, KC_WH_D, KC_BTN1, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_ENT,
+        KC_ESC,  KC_MUTE, KC_ACL0, KC_WH_D, KC_BTN1, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_ENT,
         _______, KC_VOLD, XXXXXXX, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_PGUP, KC_PSCR, KC_PGDN, KC_MNXT, _______,
                                             _______, _______, _______, _______
     ),
