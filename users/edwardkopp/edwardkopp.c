e6178d4c57cd799b76f92e61ead637ef68bce1a0
Edward Kopp <edward@edwardkopp.com>
2024-07-03 01:40:01 -0400

    specify custom pointing device driver

--- users/edwardkopp/edwardkopp.c
+++ users/edwardkopp/edwardkopp.c
@@ -232,3 +232,10 @@ void matrix_scan_user(void)
         pointing_device_send();
     }
 }
+
+
+// Required functions for using pointing device functionality
+void pointing_device_driver_init(void) {}
+report_mouse_t pointing_device_driver_get_report(report_mouse_t report) { return report; }
+uint16_t pointing_device_driver_get_cpi(void) { return 0; }
+void pointing_device_driver_set_cpi(uint16_t cpi) {}
