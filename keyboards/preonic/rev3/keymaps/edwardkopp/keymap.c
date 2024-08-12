1c255bed4b837d5c92999dbdf8585708615b2f00
Edward Kopp <edward@edwardkopp.com>
2024-08-12 07:02:03 -0400

    remove no longer existing custom keys from preonic

--- keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
+++ keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        EK_LMOD, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, EK_RMOD,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EK_SYM,  KC_SPC,  KC_SPC,  EK_NAV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_SYMBOL] = LAYOUT_preonic_grid(
