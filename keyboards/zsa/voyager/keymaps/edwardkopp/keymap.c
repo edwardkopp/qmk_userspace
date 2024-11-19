ba9b6a2ba6c93266428ea1931416fba981018637
Edward Kopp <edward@edwardkopp.com>
2024-11-19 01:32:30 -0500

    expand functionality of mod layer, and change access to it

--- keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
+++ keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
@@ -26,34 +26,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            EK_SYM,  EK_MOD,  KC_SPC,  EK_NAV
+                                            EK_SYM,  KC_SPC,  KC_SPC,  EK_NAV
     ),
     [_SYMBOL] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
         _______, KC_BSLS, KC_PIPE, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, _______,
-                                            _______, _______, _______, _______
+                                            _______, _______, EK_MOD,  _______
     ),
     [_NAV] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_MPRV, KC_DEL,
-        KC_PAUS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_ENT,
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, KC_PGDN, KC_MNXT, _______,
-                                            _______, _______, _______, _______
+        KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  XXXXXXX, KC_DEL,
+        KC_PAUS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, KC_ENT,
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, KC_PGDN, XXXXXXX, _______,
+                                            _______, EK_MOD,  _______, _______
     ),
     [_RAT] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        XXXXXXX, KC_VOLU, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_MS_U, KC_END,  KC_MPRV, KC_DEL,
-        KC_ESC,  KC_MUTE, KC_ACL1, KC_WH_D, KC_BTN1, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_ENT,
-        _______, KC_VOLD, XXXXXXX, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_PGUP, KC_PSCR, KC_PGDN, KC_MNXT, _______,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_MS_U, KC_END,  XXXXXXX, KC_DEL,
+        KC_ESC,  XXXXXXX, KC_ACL1, KC_WH_D, KC_BTN1, EK_MOD,  EK_MOD,  KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, KC_ENT,
+        _______, XXXXXXX, XXXXXXX, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_PGUP, KC_PSCR, KC_PGDN, XXXXXXX, _______,
                                             _______, _______, _______, _______
     ),
     [_MOD] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_LGUI, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                                            _______, _______, KC_CAPS, _______
+        _______, _______, _______, KC_LGUI, KC_CAPS, _______, _______, KC_CAPS, KC_LGUI, _______, _______, _______,
+        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, _______,
+        _______, _______, KC_VOLU, KC_MUTE, KC_VOLD, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
+                                            _______, _______, _______, _______
     )
 };
