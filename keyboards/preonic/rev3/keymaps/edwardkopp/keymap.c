732d288f88b4fb7a47cdebb9a0085cd468e86ca0
Edward Kopp <edward@edwardkopp.com>
2024-11-20 08:50:37 -0500

    update preonic keymap to match voyager

--- keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
+++ keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
@@ -22,38 +22,38 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 // The keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_preonic_grid(
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EK_SYM,  KC_SPC,  KC_SPC,  EK_NAV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+        XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
+        XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EK_LEFT, KC_SPC,  KC_SPC,  EK_RGHT  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
-    [_SYMBOL] = LAYOUT_preonic_grid(
+    [_LMOD] = LAYOUT_preonic_grid(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ENT,
-        _______, KC_BSLS, KC_PIPE, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, KC_PIPE, KC_VOLU, KC_MUTE, KC_VOLD, KC_TILD, KC_GRV,  KC_1,    KC_2,    KC_3,    _______, _______,
+        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, KC_DQUO, KC_QUOT, KC_4,    KC_5,    KC_6,    KC_0,    _______,
+        _______, KC_QUES, KC_AMPR, KC_LGUI, KC_LT,   KC_GT,   KC_PERC, KC_7,    KC_8,    KC_9,    KC_DLR,  _______,
+        _______, _______, _______, _______, _______, EK_RAT,  EK_RAT,  _______, _______, _______, _______, _______
+    ),
+    [_RMOD] = LAYOUT_preonic_grid(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_BSLS, KC_AT,   KC_EQL,  KC_LCBR, KC_RCBR, KC_PLUS, KC_MPRV, KC_MPLY, KC_MNXT, KC_COLN, _______,
+        _______, _______, KC_EXLM, KC_MINS, KC_LPRN, KC_RPRN, KC_UNDS, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI, _______,
+        _______, _______, KC_HASH, KC_TAB,  KC_LBRC, KC_RBRC, KC_ASTR, KC_CIRC, KC_LGUI, _______, _______, _______,
+        _______, _______, _______, _______, _______, EK_RAT,  EK_RAT,  _______, _______, _______, _______, _______
     ),
     [_NAV] = LAYOUT_preonic_grid(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_MPRV, KC_DEL,
-        KC_PAUS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_ENT,
-        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_PGUP, XXXXXXX, KC_PGDN, KC_MNXT, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_INS,  KC_DEL,  KC_HOME, KC_UP,   KC_END,  XXXXXXX, _______,
+        _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_ESC,  KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______,
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_CAPS, KC_ENT,  KC_PGUP, KC_PAUS, KC_PGDN, XXXXXXX, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
     [_RAT] = LAYOUT_preonic_grid(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_VOLU, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_MS_U, KC_END,  KC_MPRV, KC_DEL,
-        KC_ESC,  KC_MUTE, KC_ACL1, KC_WH_D, KC_BTN1, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_MPLY, KC_ENT,
-        _______, KC_VOLD, XXXXXXX, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_PGUP, KC_PSCR, KC_PGDN, KC_MNXT, _______,
+        _______, XXXXXXX, XXXXXXX, KC_WH_U, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_MS_U, KC_END,  XXXXXXX, _______,
+        _______, XXXXXXX, KC_ACL1, KC_WH_D, KC_BTN1, XXXXXXX, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, _______,
+        _______, XXXXXXX, XXXXXXX, KC_BTN3, KC_BTN2, XXXXXXX, XXXXXXX, KC_PGUP, KC_PSCR, KC_PGDN, XXXXXXX, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-    [_MOD] = LAYOUT_preonic_grid(
-        _______, _______, _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_RCTL, KC_RSFT, KC_RALT, KC_LGUI, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, KC_CAPS, _______, _______, _______, _______, _______
     )
 };
