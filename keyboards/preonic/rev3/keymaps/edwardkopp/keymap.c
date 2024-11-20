542230fedd1f7b754905761c60117b645c3dba31
Edward Kopp <edward@edwardkopp.com>
2024-11-20 08:55:20 -0500

    add missing comma

--- keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
+++ keyboards/preonic/rev3/keymaps/edwardkopp/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
         XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, XXXXXXX,
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EK_LEFT, KC_SPC,  KC_SPC,  EK_RGHT  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, EK_LEFT, KC_SPC,  KC_SPC,  EK_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
     [_LMOD] = LAYOUT_preonic_grid(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
