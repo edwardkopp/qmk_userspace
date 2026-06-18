6372b904e3857e915c942983f05873b91db7dd98
Edward Kopp <edward@edwardkopp.com>
2026-06-18 19:29:14 -0400

    replace kc_int5 with kc_int4

--- keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
+++ keyboards/zsa/voyager/keymaps/edwardkopp/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_voyager(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         KC_INS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX, XXXXXXX, KC_HOME, KC_UP,   KC_END,  KC_MPRV, KC_DEL,
-        KC_PAUS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, KC_INT5, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_ENT,
+        KC_PAUS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX, KC_INT4, KC_LEFT, KC_DOWN, KC_RGHT, KC_MPLY, KC_ENT,
         _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, KC_PGUP, KC_CAPS, KC_PGDN, KC_MNXT, _______,
                                             _______, _______, _______, _______
     ),
