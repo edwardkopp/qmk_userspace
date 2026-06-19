bcf812a17bc0ec81a25ae38bae59a427c6e82910
Edward Kopp <edward@edwardkopp.com>
2026-06-18 22:34:26 -0400

    Revert "use custom keycodes for mods"
    This reverts commit c16ff95b

--- users/edwardkopp/edwardkopp.h
+++ users/edwardkopp/edwardkopp.h
@@ -21,7 +21,8 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 
 // The layers
-enum rainbow_layer_cake {
+enum rainbow_layer_cake
+{
     _QWERTY,
     _SYMBOL,
     _NAV,
@@ -30,29 +31,7 @@ enum rainbow_layer_cake {
 };
 
 
-// Custom keys primarily for mods layer
-enum custom_key_codes {
-    EK_CUSTOMS_START = SAFE_RANGE,
-
-    // Left mods
-    EK_LMOD_START,
-    EK_LCTL,
-    EK_LGUI,
-    EK_LALT,
-    EK_LSFT,
-    EK_LMOD_END,
-
-    // Right mods
-    EK_RMOD_START,
-    EK_RCTL,
-    EK_RGUI,
-    EK_RALT,
-    EK_RSFT,
-    EK_RMOD_END
-};
-
-
 // Keycodes for exploration of the rainbow layer cake
 #define EK_SYM MO(_SYMBOL)
 #define EK_NAV MO(_NAV)
-#define EK_LSPC MO(_MOD)
+#define EK_LSPC LT(_MOD, KC_SPC)
